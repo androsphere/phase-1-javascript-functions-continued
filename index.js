ee62c3e26618b672f9d8f7951493cb10d656afbd
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-13 22:54:26 -0700

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(something = "roller-skate"){
+    return `This Saturday, I want to ${something}!`;
+}
+const mondayWork = function(something = "go to the office"){
+    return `This Monday, I will ${something}.`;
+}
+function wrapAdjective(flair)
+{
+    let message;
+    return function(message = "special"){
+        return `You are ${flair}${message}${flair}!`;
+    }
+}
